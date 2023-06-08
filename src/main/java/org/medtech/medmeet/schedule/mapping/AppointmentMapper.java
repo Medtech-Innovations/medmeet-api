1303474969a42eebe86de38f6292ddd1cedfcd44
sayufu <lilifuye72@gmail.com>
2023-06-08 14:27:08 -0500

    feat: add appointment mapper

---
+++ src/main/java/org/medtech/medmeet/schedule/mapping/AppointmentMapper.java
@@ -0,0 +1,23 @@
+package org.medtech.medmeet.schedule.mapping;
+
+import org.medtech.medmeet.schedule.domain.model.entity.Appointment;
+import org.medtech.medmeet.schedule.resource.CreateAppointmentResource;
+import org.medtech.medmeet.shared.mapping.EnhancedModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.io.Serializable;
+
+public class AppointmentMapper implements Serializable {
+    @Autowired
+    EnhancedModelMapper mapper;
+
+    public Appointment toModel(CreateAppointmentResource resource) {
+        return this.mapper.map(resource, Appointment.class);
+    }
+
+
+
+
+
+
+}
