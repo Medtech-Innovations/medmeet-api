6b1ce37b4fbcac5a7d2f6f22c1534ea44c25da23
sayufu <lilifuye72@gmail.com>
2023-06-09 14:37:31 -0500

    fix: hotfix for mapper names and application configuration

--- src/main/java/org/medtech/medmeet/support/mapping/QuestionMapper.java
+++ src/main/java/org/medtech/medmeet/support/mapping/QuestionMapper.java
@@ -1,17 +1,18 @@
 package org.medtech.medmeet.support.mapping;
 
+import org.medtech.medmeet.shared.mapping.EnhancedModelMapper;
 import org.medtech.medmeet.support.domain.model.entity.Question;
 import org.medtech.medmeet.support.resource.CreateQuestionResource;
 import org.medtech.medmeet.support.resource.QuestionResource;
 import org.medtech.medmeet.support.resource.UpdateQuestionResource;
-import org.medtech.medmeet.support.shared.mapping.EnhancedModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.Serializable;
 
 public class QuestionMapper implements Serializable {
 
-    @Autowired EnhancedModelMapper mapper;
+    @Autowired
+    EnhancedModelMapper mapper;
     public Question toModel(CreateQuestionResource question){
         return this.mapper.map(question,Question.class);
     }
--- src/main/java/org/medtech/medmeet/support/service/QuestionImpl.java
+++ src/main/java/org/medtech/medmeet/support/service/QuestionImpl.java
@@ -2,12 +2,11 @@ package org.medtech.medmeet.support.service;
 
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.Validator;
+import org.medtech.medmeet.shared.exception.ResourceNotFoundException;
+import org.medtech.medmeet.shared.exception.ResourceValidationException;
 import org.medtech.medmeet.support.domain.model.entity.Question;
 import org.medtech.medmeet.support.domain.persistence.QuestionRepository;
 import org.medtech.medmeet.support.domain.service.QuestionService;
-import org.medtech.medmeet.support.shared.Constant;
-import org.medtech.medmeet.support.shared.exception.ResourceNotFoundException;
-import org.medtech.medmeet.support.shared.exception.ResourceValidationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,6 +17,8 @@ import java.util.Set;
 
 @Service
 public class QuestionImpl implements QuestionService {
+
+    private static final String ENTITY = "Question";
 
     @Autowired
     private QuestionRepository questionRepository;
@@ -47,7 +48,7 @@ public class QuestionImpl implements QuestionService {
     public Question save(Question question){
         Set<ConstraintViolation<Question>>violations=validator.validate(question);
         if(!violations.isEmpty()){
-            throw new ResourceValidationException(Constant.QUESTION_ENTITY,violations);
+            throw new ResourceValidationException(ENTITY,violations);
         }
         return questionRepository.save(question);
     }
