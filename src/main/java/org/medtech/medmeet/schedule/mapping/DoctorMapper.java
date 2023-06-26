0435ab61496a198a0b3a19413484dd38d50fa39f
sayufu <lilifuye72@gmail.com>
2023-06-26 17:21:26 -0500

    feat: add doctor entity mapper

---
+++ src/main/java/org/medtech/medmeet/schedule/mapping/DoctorMapper.java
@@ -0,0 +1,28 @@
+package org.medtech.medmeet.schedule.mapping;
+
+import org.medtech.medmeet.schedule.domain.model.entity.Appointment;
+import org.medtech.medmeet.schedule.domain.model.entity.Doctor;
+import org.medtech.medmeet.schedule.resource.appointment.AppointmentResource;
+import org.medtech.medmeet.schedule.resource.appointment.UpdateAppointmentResource;
+import org.medtech.medmeet.schedule.resource.doctor.CreateDoctorResource;
+import org.medtech.medmeet.schedule.resource.doctor.DoctorResource;
+import org.medtech.medmeet.schedule.resource.doctor.UpdateDoctorResource;
+import org.medtech.medmeet.shared.mapping.EnhancedModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.io.Serializable;
+
+public class DoctorMapper implements Serializable {
+    @Autowired
+    EnhancedModelMapper mapper;
+
+    public Doctor toModel(CreateDoctorResource resource) {
+        return this.mapper.map(resource, Doctor.class);
+    }
+    public Doctor toModel(UpdateDoctorResource resource) {
+        return this.mapper.map(resource, Doctor.class);
+    }
+    public DoctorResource toResource(Doctor doctor) {
+        return this.mapper.map(doctor, DoctorResource.class);
+    }
+}
