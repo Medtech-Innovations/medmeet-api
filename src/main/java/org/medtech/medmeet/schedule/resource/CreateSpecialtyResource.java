eb0e20080558a4fac01a4de702bf5279f96a73cb
sayufu <lilifuye72@gmail.com>
2023-06-26 17:00:35 -0500

    feat: add specialty entity resources

---
+++ src/main/java/org/medtech/medmeet/schedule/resource/CreateSpecialtyResource.java
@@ -0,0 +1,17 @@
+package org.medtech.medmeet.schedule.resource;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Getter
+@Setter
+@With
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateSpecialtyResource {
+
+    @NotNull
+    @Size(max = 128)
+    private String name;
+}
---
+++ src/main/java/org/medtech/medmeet/schedule/resource/SpecialtyResource.java
@@ -0,0 +1,13 @@
+package org.medtech.medmeet.schedule.resource;
+
+import lombok.*;
+
+@Getter
+@Setter
+@With
+@NoArgsConstructor
+@AllArgsConstructor
+public class SpecialtyResource {
+    private Integer id;
+    private String name;
+}
