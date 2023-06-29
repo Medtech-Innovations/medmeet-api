ed84dd4ba99dc303aa986b7cf1590d6c220b10cc
sayufu <lilifuye72@gmail.com>
2023-06-29 13:05:44 -0500

    feat: add cors configuration

---
+++ src/main/java/org/medtech/medmeet/shared/configuration/CorsConfiguration.java
@@ -0,0 +1,19 @@
+package org.medtech.medmeet.shared.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
